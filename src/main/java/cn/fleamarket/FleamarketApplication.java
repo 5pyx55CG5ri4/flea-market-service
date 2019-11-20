687dfca150ab88f285496f445a15e1b76fd43987
Oylf <1125446549@qq.com>
2019-11-20 19:21:36 +0800

    oylf

--- src/main/java/cn/fleamarket/FleamarketApplication.java
+++ src/main/java/cn/fleamarket/FleamarketApplication.java
@@ -1,16 +1,29 @@
 package cn.fleamarket;
 
-
+import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("cn.fleamarket.mapper")
 @EnableSwagger2
-public class FleamarketApplication {
+public class FleamarketApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(FleamarketApplication.class, args);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+
 }
