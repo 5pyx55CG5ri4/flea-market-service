96d2014ba69ff752282d4b7452fc32748622f3f4
zhuliyou <1559422960@qq.com>
2023-09-05 18:52:56 +0800

    表结构

--- src/main/resources/db/face.sql
+++ src/main/resources/db/face.sql
@@ -1,141 +1,49 @@
-/*
- Navicat Premium Data Transfer
+create table sys_favorites
+(
+    id          bigint auto_increment
+        primary key,
+    product_id  bigint                              null comment '商品id',
+    user_id     bigint                              null comment '用户id',
+    create_time timestamp default CURRENT_TIMESTAMP null comment '创建时间'
+)
+    comment '收藏表';
 
- Source Server         : 乌班图子系统数据库
- Source Server Type    : MySQL
- Source Server Version : 80022
- Source Host           : localhost:3306
- Source Schema         : face
+create table sys_message
+(
+    id          bigint auto_increment
+        primary key,
+    product_id  bigint                              null comment '商品id',
+    user_id     bigint                              null comment '用户id',
+    create_time timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    message     varchar(1024)                       null comment '消息内容'
+)
+    comment '消息表';
 
- Target Server Type    : MySQL
- Target Server Version : 80022
- File Encoding         : 65001
+create table sys_product
+(
+    id               bigint auto_increment
+        primary key,
+    title            varchar(255)                        null comment '标题',
+    content          varchar(1024)                       null comment '商品内容',
+    original_price   decimal(12, 2)                      null comment '原价',
+    sale_price       decimal(12, 2)                      null comment '出售价格',
+    create_time      timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    create_by        bigint                              null comment '创建人',
+    viewed           int       default 0                 null comment '浏览次数',
+    collection_count int       default 0                 null comment '收藏次数',
+    img_url          varchar(255)                        null comment '图片链接地址',
+    state            char      default '1'               null comment '状态 0 下架 1 上架'
+)
+    comment '商品表';
 
- Date: 22/04/2021 16:52:29
-*/
+create table sys_user
+(
+    id          bigint auto_increment
+        primary key,
+    nick_name   varchar(45)                         null comment '昵称',
+    user_name   varchar(255)                        null comment '用户名/邮箱号/手机号',
+    create_time timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    state       char      default '1'               null comment '状态 0 冻结 1 有效'
+)
+    comment '用户表';
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for f_code
--- ----------------------------
-DROP TABLE IF EXISTS `f_code`;
-CREATE TABLE `f_code`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `code_time` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for f_favorites
--- ----------------------------
-DROP TABLE IF EXISTS `f_favorites`;
-CREATE TABLE `f_favorites`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `product_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `state` int(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for f_image
--- ----------------------------
-DROP TABLE IF EXISTS `f_image`;
-CREATE TABLE `f_image`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `table_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `table_id` int(0) NULL DEFAULT NULL,
-  `img_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `isdetault` int(0) NULL DEFAULT NULL,
-  `about` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for f_message
--- ----------------------------
-DROP TABLE IF EXISTS `f_message`;
-CREATE TABLE `f_message`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `t_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `f_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `text` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `time` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for f_order
--- ----------------------------
-DROP TABLE IF EXISTS `f_order`;
-CREATE TABLE `f_order`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `order_no` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `payable_fee` decimal(18, 4) NULL DEFAULT NULL,
-  `real_fee` decimal(18, 4) NULL DEFAULT NULL,
-  `payment_way` int(0) NULL DEFAULT NULL,
-  `pay_state` int(0) NULL DEFAULT NULL,
-  `order_state` int(0) NULL DEFAULT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `pay_time` datetime(0) NULL DEFAULT NULL,
-  `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `product_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for f_product
--- ----------------------------
-DROP TABLE IF EXISTS `f_product`;
-CREATE TABLE `f_product`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `bprice` decimal(18, 4) NULL DEFAULT NULL,
-  `price` decimal(18, 4) NULL DEFAULT NULL,
-  `times` int(0) NULL DEFAULT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `user_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `is_show` int(0) NULL DEFAULT NULL,
-  `is_del` int(0) NULL DEFAULT NULL,
-  `count` int(0) NULL DEFAULT NULL,
-  `sc` int(0) NULL DEFAULT NULL,
-  `img_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for f_user
--- ----------------------------
-DROP TABLE IF EXISTS `f_user`;
-CREATE TABLE `f_user`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `pass_word` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `tel` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `credit` int(0) NULL DEFAULT NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `state` int(0) NULL DEFAULT NULL,
-  `wxopenid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `qqopenid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `aliopenid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `money` decimal(18, 4) NULL DEFAULT NULL,
-  `room_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-alter table f_product alter column is_show set default 1;
-
-alter table f_product alter column is_del set default 0;
-
-alter table f_product alter column count set default 0;
-
-alter table f_product alter column sc set default 0;
-
-
-SET FOREIGN_KEY_CHECKS = 1;
